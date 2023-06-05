911ef29a845f8671eb331c59f618d0d763ff583a
Emmy-Iraturinzee <iraturinzeemanuel@gmial.com>
2023-06-05 16:15:03 +0200

    new

--- modal-04/js/main.js
+++ modal-04/js/main.js
@@ -1,5 +1,11 @@
 (function($) {
 
+	window.location = 'whatsapp://send?text='+encodeURIComponent(imageURL);
+	var fakeLink = document.createElement('a');
+fakeLink.setAttribute('href', 'whatsapp://send?text='+encodeURIComponent(imageURL));
+fakeLink.setAttribute('data-action', 'share/whatsapp/share');
+fakeLink.click();
+
 	"use strict";
 
 	$('[data-toggle="tooltip"]').tooltip()
