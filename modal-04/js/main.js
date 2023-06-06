70591f59b90583f514a98ba054c0ec710bd5d73d
Emmy-Iraturinzee <iraturinzeemanuel@gmial.com>
2023-06-06 15:32:06 +0200

    new

--- modal-04/js/main.js
+++ modal-04/js/main.js
@@ -1,10 +1,22 @@
 (function($) {
-
+//whatsapp link
 	window.location = 'whatsapp://send?text='+encodeURIComponent(imageURL);
 	var fakeLink = document.createElement('a');
 fakeLink.setAttribute('href', 'whatsapp://send?text='+encodeURIComponent(imageURL));
 fakeLink.setAttribute('data-action', 'share/whatsapp/share');
 fakeLink.click();
+// singin up
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
 
 	"use strict";
 
